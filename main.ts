0518a3893787774f09cfa9afeb453cf597094bff
AOBU001 <95912768+AOBU001@users.noreply.github.com>
2021-12-10 20:26:38 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,9 @@
-let スプライト = game.createSprite(4, 0)
 basic.forever(function () {
-    スプライト.move(1)
-    basic.pause(50)
-    スプライト.ifOnEdgeBounce()
-    if (スプライト.get(LedSpriteProperty.X) == 0 || スプライト.get(LedSpriteProperty.X) == 4) {
-        スプライト.change(LedSpriteProperty.Y, 1)
-        basic.pause(100)
-    }
-    if (スプライト.get(LedSpriteProperty.X) == 4) {
-        if (スプライト.get(LedSpriteProperty.Y) == 4) {
-            スプライト.set(LedSpriteProperty.X, 4)
-            スプライト.set(LedSpriteProperty.Y, 0)
-        }
-    }
+    images.createBigImage(`
+        # . # . # . # . # .
+        . # . # . # . # . #
+        # . # . # . # . # .
+        . # . # . # . # . #
+        # . # . # . # . # .
+        `).scrollImage(1, 100)
 })
